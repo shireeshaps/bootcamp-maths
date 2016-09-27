1c650731be8ea7162839cbe74036652292360c7b
Ranjeet Singh <mail@raeoks.com>
2016-09-27 14:54:10 +0530

    Adds number to Number class reader

--- lib/maths/number.rb
+++ lib/maths/number.rb
@@ -1,24 +1,28 @@
 class Maths::Number
+  attr_reader :number
+
   def initialize(number)
     @number = number
   end
 
-  def number
-    @number
-  end
-
   def factorial
-    if (@number < 0)
+    if (self.number < 0)
       Float::INFINITY
-    elsif (@number == 0)
+    elsif (self.number == 0)
       1
     else
       # HACK: Should be simplified
-      number = @number
-      @number = @number - 1
+      number = self.number
+      self.number = self.number - 1
       number * self.factorial
     end
   end
 
   alias_method :n!, :factorial
+
+  private
+
+  def number=(new_number)
+    @number = new_number
+  end
 end
